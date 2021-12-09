4127c40d21770fb5e8f087924f2b4294b7f42163
Ize Phanthakarn <89945495+izephanthakarn@users.noreply.github.com>
2021-12-09 17:01:00 +0700

    Create Lecture50_Phanthakarn_K.py

---
+++ Lecture50_Phanthakarn_K.py
@@ -0,0 +1,16 @@
+num1 = 7
+num2 = 4
+
+def plus(x,y):
+    print("Plus = ",x+y)
+def minus(x,y):
+    print("Minus = ",x-y)
+def multiply(x,y):
+    print("Multiply = ",x*y)
+def divide(x,y):
+    print("Divide = ",int(x/y))
+
+plus(num1, num2)
+minus(num1, num2)
+multiply(num1, num2)
+divide(num1, num2)
