fa5ea724f27cef2d5211f48ee00288e84fec4432
Ize Phanthakarn <89945495+izephanthakarn@users.noreply.github.com>
2021-12-09 14:59:09 +0700

    Create Exercise4_Phanthakarn_K.py

---
+++ Exercise4_Phanthakarn_K.py
@@ -0,0 +1,10 @@
+FE = 78.8   #Foundation English Score
+GB = 82.5   #General Business Score
+IC = 87.0   #Introduction to Computer Systems Score
+CP = 74.3   #Computer Programming Score
+
+print("--- Your Score ---")
+print("Foundation English : ", FE)
+print("General Business : ", GB)
+print("Introduction to Computer Systems : ", IC)
+print("Computer Programming : ", CP)
